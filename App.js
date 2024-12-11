27b2dbae20fdd6c696f51959a2a407ce7b1eab54
aphithun duangchankloi <127219087+pond1111@users.noreply.github.com>
2024-12-11 10:13:01 +0700

    Add AnothorLogin

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
+import AnotherLoginScreen from './screens/AnotherLoginScreen';
 import MainTabs from './navigations/MainTabs'; 
 
 const Stack = createStackNavigator();
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }} />
+        <Stack.Screen name="AnotherLoginScreen" component={AnotherLoginScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
