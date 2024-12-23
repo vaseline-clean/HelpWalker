e5d358e92bad81bc7f50be8e7892f44b1d5e1249
aphithun duangchankloi <127219087+pond1111@users.noreply.github.com>
2024-12-23 18:47:45 +0700

    แก้หน้าApp

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import AnotherLoginScreen from './screens/AnotherLoginScreen';
 import MainTabs from './navigations/MainTabs'; 
+import SindupScreen from './screens/SindupScreen';
 import MissionDetailsScreen from './screens/MissionDetailsScreen'; // Import หน้ารายละเอียดภารกิจ
 import FeedScreen from './screens/FeedScreen'; // Import FeedScreen
-import SindupScreen from './screens/SindupScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,10 +14,42 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }} />
-        <Stack.Screen name="AnotherLoginScreen" component={AnotherLoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="SindupScreen" component={SindupScreen} options={{ headerShown: false }} />
+        {/* หน้าจอ Login */}
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+        name="AnotherLoginScreen" 
+        component={AnotherLoginScreen} 
+        options={{ headerShown: false }} />
+        
+        <Stack.Screen 
+        name="SindupScreen" 
+        component={SindupScreen} 
+        options={{ headerShown: false }} />
+
+        {/* หน้าหลัก MainTabs */}
+        <Stack.Screen 
+          name="MainTabs" 
+          component={MainTabs} 
+          options={{ headerShown: false }} 
+        />
+
+        {/* หน้าฟีด (FeedScreen) */}
+        <Stack.Screen 
+          name="FeedScreen" 
+          component={FeedScreen} 
+          options={{ title: 'ฟีดภารกิจ', headerShown: true }} 
+        />
+
+        {/* หน้ารายละเอียดภารกิจ (MissionDetailsScreen) */}
+        <Stack.Screen 
+          name="MissionDetails" 
+          component={MissionDetailsScreen} 
+          options={{ title: 'รายละเอียดภารกิจ', headerShown: true }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
